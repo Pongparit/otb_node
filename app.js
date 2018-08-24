3676843e1adf0322f97201862149a0a3af257d89
Earl <Pongparit.pa@gmail.com>
2018-08-24 15:04:05 +0700

    Initial api route

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 /* define library */
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 /* define variable */
 const port = 3000
-const indexRoute = require("./routers/index")
+const indexRoute = require('./routers/index')
+const apiRoute = require('./routers/api/index')
 
 /* define controller */
 
 /* add route */
 app.use('/', indexRoute)
-
+app.use('/api/', apiRoute)
 
 /* setup port server running */
-app.listen(port, () => console.info(`Server is running on port: ${port}`));
+app.listen(port, () => console.info(`Server is running on port: ${port}`))
